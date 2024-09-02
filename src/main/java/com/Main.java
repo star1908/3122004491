2ee6d9022ad727bb243c5b69d8063aa9de21b08c
star <13782156+star1908@user.noreply.gitee.com>
2024-09-02 19:58:32 +0800

    写入文件功能

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,9 +1,16 @@
 package com;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        if(args.length < 3){
+            throw new Exception("绝对路径不足三个，自动结束");
+        }
+
         ReadFile readFile = new ReadFile();
         String string1 = readFile.Read(args[0]);
         String string2 = readFile.Read(args[1]);
+
+        OutputFile outputFile = new OutputFile();
+        outputFile.Output(args[2],0.88);
     }
 }
--- src/main/java/com/OutputFile.java
+++ src/main/java/com/OutputFile.java
@@ -1,6 +1,20 @@
 package com;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class OutputFile {
-    public OutputFile() {
+    public void Output(String url,Number ratio) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(url);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+
+            outputStreamWriter.write("论文查重率为 " + ratio);
+            outputStreamWriter.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException("文件写入失败", e);
+        }
     }
 }
